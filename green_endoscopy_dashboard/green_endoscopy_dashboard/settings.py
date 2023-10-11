8d34d739f6b345e081a69984bdec8b03380553c8
Thomas J. Lux <31990770+Maddonix@users.noreply.github.com>
2023-10-11 03:08:53 +0200

    rm migrations

--- green_endoscopy_dashboard/green_endoscopy_dashboard/settings.py
+++ green_endoscopy_dashboard/green_endoscopy_dashboard/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = True
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 DEVELOPMENT_MODE = True
 
--- green_endoscopy_dashboard/product_emissions/views.py
+++ green_endoscopy_dashboard/product_emissions/views.py
@@ -29,8 +29,7 @@ def home_view(request):
     if not ref_product.emission_factor_total:
         for product_group in product_groups:
             print(f"set_emission_factors for {product_group}")
-            for ref_product in product_group.reference_product.all():
-                ref_product.set_emission_factors()
+            ref_product.set_emission_factors()
 
 
     serializer = ProductGroupSerializer(product_groups, many=True)
